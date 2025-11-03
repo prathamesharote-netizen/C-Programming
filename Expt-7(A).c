331c84a97f7f492dfbb796ebec9516daf3282066
prathamesharote-netizen <prathamesharote@eng.rizvi.edu.in>
2025-11-03 16:47:40 +0530

    Create Expt-7(A).c

---
+++ Expt-7(A).c
@@ -0,0 +1,22 @@
+int main() {
+    int n, i, arr[100], largest;
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    largest = arr[0];
+    for (i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    
+    printf("Largest element = %d\n", largest);
+    
+    return 0;
+}
