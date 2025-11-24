4058e2b34113591767e4621c32a25fa657323e4b
prathamesharote-netizen <prathamesharote@eng.rizvi.edu.in>
2025-11-24 13:47:22 +0530

    Create Expt 9.c

---
+++ Expt 9.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+/* 
+ * Name: Prathamesh Lakshman Arote
+ * Class: Civil
+ * Roll No: 19
+ * UIN: 2510022
+ */
+
+struct student_record {
+    char name[50];
+    int roll_number;
+    float total_marks;
+};
+
+int main() {
+    struct student_record students[5];
+    int i;
+
+    printf("Enter details for 5 students:\n");
+
+    for (i = 0; i < 5; i++) {
+        printf("\nStudent %d:\n", i + 1);
+        printf("Enter name: ");
+        scanf("%49s", students[i].name); // Use %49s to prevent buffer overflow
+        printf("Enter roll number: ");
+        scanf("%d", &students[i].roll_number);
+        printf("Enter total marks: ");
+        scanf("%f", &students[i].total_marks);
+    }
+
+    struct student_record topper = students[0];
+
+    for (i = 1; i < 5; i++) {
+        if (students[i].total_marks > topper.total_marks) {
+            topper = students[i];
+        }
+    }
+
+    printf("\n--- Topper Information ---\n");
+    printf("Name: %s\n", topper.name);
+    printf("Roll Number: %d\n", topper.roll_number);
+    printf("Total Marks: %.2f\n", topper.total_marks);
+
+    return 0;
+}
