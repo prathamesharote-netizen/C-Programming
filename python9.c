8564d6362b36c5ee27d6650aa677ca8c6e47fb63
prathamesharote-netizen <prathamesharote@eng.rizvi.edu.in>
2026-02-15 20:04:53 +0530

    Create python9.c

---
+++ python9.c
@@ -0,0 +1,9 @@
+/* Name : Prathamesh Lakshman Arote
+UIN : 251C022
+Branch : Civil
+Div : B*/
+n= int(input("Enter a number:"))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print("Factorial is:", fact)
