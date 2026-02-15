ca93a47ff3a200f114b5af00161e7a648e9820f8
prathamesharote-netizen <prathamesharote@eng.rizvi.edu.in>
2026-02-15 19:36:54 +0530

    Create python4.c

---
+++ python4.c
@@ -0,0 +1,11 @@
+/* Name : Prathamesh Lakshman Arote
+UIN : 251C022
+Branch : Civil
+Div : B*/
+name=input("enter your name:")
+print(name)
+p = float(input("Enter principal amount: "))
+r = float(input("Enter rate of interest: "))
+t = float(input("Enter time(in years): "))
+si = (p * r * t) / 100
+print("Simple interest=", si)
